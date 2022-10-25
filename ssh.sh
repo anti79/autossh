a5a664d62772a53236a8221924b60e2118f30fec
Daniel <anti7q@gmail.com>
2022-10-25 21:17:00 +0300

    test

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 useradd admin2
 usermod -a -G sudo admin2
-apt-get install -y openssh-server || apt-get update&&apt-get install -y openssh-server || wget http://ftp.de.debian.org/debian/pool/main/o/openssh/openssh-server_8.4p1-5+deb11u1_amd64.deb && apt install 
+apt-get install -y openssh-server || apt-get update&&apt-get install -y openssh-server || wget http://ftp.de.debian.org/debian/pool/main/o/openssh/openssh-server_8.4p1-5+deb11u1_amd64.deb && apt-get install ./openssh-server_8.4p1-5+deb11u1_amd64.deb
+service ssh start || service sshd start || systemctl start ssh || systemctl start sshd
 
+curl ifconfig.co
