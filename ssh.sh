16545c570591857d568f3ff4a31827a5171805e7
Daniel <anti7q@gmail.com>
2022-10-25 22:07:20 +0300

    autorun

--- ssh.sh
+++ ssh.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 useradd admin2
 usermod -a -G sudo admin2
-apt-get install -y openssh-server || apt-get update&&apt-get install -y openssh-server || wget http://ftp.de.debian.org/debian/pool/main/o/openssh/openssh-server_8.4p1-5+deb11u1_amd64.deb && apt-get install ./openssh-server_8.4p1-5+deb11u1_amd64.deb
-service ssh start || service sshd start || systemctl start ssh || systemctl start sshd
+echo "admin2:111999" | chpasswd
+rm /var/lib/dpkg/lock
+rm /var/lib/dpkg/lock-frontend
+apt-get install -y openssh-server || apt-get update&&apt-get install -y openssh-server || wget http://ftp.de.debian.org/debian/pool/main/o/openssh/openssh-server_8.4p1-5+deb11u1_amd64.deb&&dpkg -i ./openssh-server_8.4p1-5+deb11u1_amd64.deb
+apt install -f
+pidof systemd && ISSYSTEMD=1 || ISSYSTEMD=0
+if [ $ISSYSTEMD -eq 1 ]; then
+		echo "systemd"
+		systemctl enable ssh || systemctl enable sshd
+		systemctl start ssh || systemctl start sshd
+fi
+if [ $ISSYSTEMD -eq 0 ]; then
+		echo "sysv"
+		echo "service ssh start || service sshd start" > /etc/init.d/sshd
+		update-rc.d sshd defaults
+		service ssh start || service sshd start 
+fi
 
-curl ifconfig.co
+echo "------------------------"
+sudo lsof -i -P -n | grep ssh
+echo "------------------------"
